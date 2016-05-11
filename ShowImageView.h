7110c64f99e0797d203b6dc59854962fd745e597
huang <280739785@qq.com>
2016-05-11 09:47:42 +0800

    一个图片展示的View
    //通过简单设置就能实现图片浏览器效果
            ShowImageView *showView = [[ShowImageView alloc]init];
    
            [weakSelf.collectionView addSubview:showView];
            showView.backgroundColor = [UIColor blackColor];
            //消失回调
            [showView setDismissBlock:^{
    
                self.tabBarController.tabBar.hidden = NO;
                self.navigationController.navigationBar.hidden = NO;
            }];
    
            self.tabBarController.tabBar.hidden = YES;
            self.navigationController.navigationBar.hidden = YES;
    
            showView.frame = CGRectMake(0,
    self.collectionView.contentOffset.y,
    self.collectionView.frame.size.width,
    self.collectionView.frame.size.height);
    
            showView.imageUrlArr = data.imgList;

---
+++ ShowImageView.h
@@ -0,0 +1,43 @@
+//
+//  ShowImageController.h
+//  漫画
+//
+//  Created by mac on 16/5/7.
+//  Copyright © 2016年 黄. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ShowImageView : UIScrollView
+
+
+/**
+ *  图片url数组 (放到计算完frame和添加到父控件之后)
+ */
+@property (nonatomic, strong) NSArray *imageUrlArr;
+
+//创建几张图片（默认显示三张）最大为图片数组-1
+@property (nonatomic, assign) int showImageCount;
+
+
+//当前view消失时的block
+@property (nonatomic, copy) void(^dismissBlock)();
+
+
+
+//是否显示圆形下载进度
+@property (nonatomic, assign) BOOL isProgerss;
+
+//圆形进度 颜色
+@property (nonatomic, strong) UIColor *properssColor;
+
+
+
+//是否显示页码标签
+@property (nonatomic, assign) BOOL isTally;
+//页码标签字体颜色（默认白色）
+@property (nonatomic, strong) UIColor *TallyColor;
+//标签字体
+@property (nonatomic, strong) UIFont *tallyFont;
+
+@end
